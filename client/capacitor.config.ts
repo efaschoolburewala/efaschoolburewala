3e1f6b35ee9c7c073fe9c513c048060c1be26d64
Muhammad Umar Ajmal <126502013+UmarAjmal@users.noreply.github.com>
2026-08-10 15:34:48 +0500

    feat: initialize Android build configuration and Capacitor project settings

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.demosmartschool.app',
+  appId: 'com.smartschool.app',
   appName: 'Demo Smart School',
   webDir: 'public',
   server: {
